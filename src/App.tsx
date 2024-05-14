21222cae65361f9b35d72b5b30ef27d086370c43
Richard <ribborges@outlook.com>
2024-05-14 15:45:16 -0300

    style: add global styles and app container styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
+import styled from "styled-components";
+import { color, font } from "./styles/theme";
+
 export default function App() {
   return (
-    <>
-    </>
+    <AppContainer>
+      <h1>Hello, World!</h1>
+    </AppContainer>
   );
 }
+
+const AppContainer = styled.div`
+  color: ${color.light};
+  font-family: ${font.family};
+  display: flex;
+  height: 100%;
+  width: 100%;
+  background-color: ${color.dark};
+`;
---
+++ src/styles/theme.ts
@@ -0,0 +1,32 @@
+// Theme variables
+
+const font = {
+    family: "'Ubuntu', sans-serif",
+    size: {
+        default: "100%"
+    },
+    weight: {
+        default: "normal"
+    }
+};
+
+const color = {
+    accent_primary: "#0000ff",
+    accent_secondary: "#7a00eb",
+    dark: "#0e0e0e",
+    dark_grey: "#222222",
+    light_grey: "#dddddd",
+    light: "#eeeeee"
+};
+
+const radius = {
+    small: "5px",
+    medium: "10px",
+    large: "20px"
+};
+
+const fx = {
+    blur: "blur(15px)",
+}
+
+export { font, color, radius, fx};
