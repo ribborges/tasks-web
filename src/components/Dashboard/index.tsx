fac33d18731ae6380ab01310083e7a2e051d50f0
Richard Borges <ribborges@outlook.com>
2025-01-29 00:52:44 -0300

    feat: replace Sidebar with Navbar in Dashboard component; improve layout and overflow handling

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,13 +1,13 @@
 import Header from "@/components/Header";
-import Sidebar from "@/components/Sidebar";
+import Navbar from "@/components/Navbar";
 
 export default function Dashboard() {
     return (
-        <div className="flex flex-col h-screen">
+        <div className="flex flex-col h-screen overflow-hidden">
             <Header />
-            <div className="flex-1 flex">
-                <Sidebar />
-                <div className="flex-1 bg-zinc-200 dark:bg-zinc-900 border border-solid border-zinc-300 dark:border-zinc-800 rounded-xl m-1">
+            <div className="flex-1 flex overflow-hidden flex-col-reverse lg:flex-row">
+                <Navbar />
+                <div className="flex-1 bg-zinc-200 dark:bg-zinc-900 border border-solid border-zinc-300 dark:border-zinc-800 rounded-xl m-0.5 lg:m-1 overflow-hidden">
 
                 </div>
             </div>
