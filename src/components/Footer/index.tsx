ca4283443f4aa1f24b305404a206f63f0e6a4b13
Richard Borges <ribborges@outlook.com>
2025-03-21 13:07:21 -0300

    style: update Footer component for improved layout and styling

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,25 +1,27 @@
+import Anchor from "@/components/Anchor";
+
 export default function Footer() {
     const currentYear = new Date().getFullYear();
 
     return (
-        <footer className="
-            flex items-center justify-center
-            bg-zinc-400 bg-opacity-50 dark:bg-zinc-800 dark:bg-opacity-50
-            border border-solid border-zinc-500 border-opacity-50 dark:border-zinc-700 dark:border-opacity-50
-            rounded-lg
-            p-4 md:p-8 lg:p-10 m-5
-            hover:scale-95
-            transition duration-500
-        ">
+        <footer
+            className="
+                text-xs
+                flex items-center justify-center
+                bg-zinc-200/50 dark:bg-zinc-900/50
+                border border-solid rounded-4xl border-zinc-300/50 dark:border-zinc-800/50
+                p-4 md:p-6 lg:p-8 m-4
+            "
+        >
             <p className="text-center text-xs md:text-sm lg:text-base">
                 {`Developed by: `}
-                <a href='https://www.linkedin.com/in/ribborges/' target={'_blank'}>Richard de Carvalho Borges</a>
+                <Anchor href='https://www.linkedin.com/in/ribborges/' target={'_blank'}>Richard de Carvalho Borges</Anchor>
                 {` with `}
-                <a href='https://nextjs.org/' target={'_blank'}>Next.js</a>{`, `}
-                <a href='https://zustand-demo.pmnd.rs/' target={'_blank'}>Zustand</a>{`, `}
-                <a href="https://www.typescriptlang.org/" target={'_blank'}>TypeScript</a>
-                {` with `}
-                <a href='https://tailwindcss.com/' target={'_blank'}>TailwindCSS</a>
+                <Anchor href='https://nextjs.org/' target={'_blank'}>Next.js</Anchor>{`, `}
+                <Anchor href='https://zustand-demo.pmnd.rs/' target={'_blank'}>Zustand</Anchor>{`, `}
+                <Anchor href="https://www.typescriptlang.org/" target={'_blank'}>TypeScript</Anchor>
+                {` and `}
+                <Anchor href='https://tailwindcss.com/' target={'_blank'}>TailwindCSS</Anchor>
                 {` | `}
                 {currentYear}
             </p>
