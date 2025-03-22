00bddb4295f07a6e37311863724efa1aa85b514e
Richard Borges <ribborges@outlook.com>
2025-03-22 02:11:06 -0300

    refactor: streamline App component by simplifying loading and redirection logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,5 @@
-'use client';
-
 import { Loading } from "@/components/Loading";
-import Redirect from "@/components/Redirect";
-import { useAuth } from "@/hooks/useAuth";
-import useCheckUser from "@/hooks/useCheckUser";
-import useLoadData from "@/hooks/useLoadData";
 
 export default function App() {
-  const { authenticated, isLoading } = useAuth();
-  const { userLoading } = useCheckUser();
-  const { dataLoading } = useLoadData();
-
-  if (isLoading || userLoading || dataLoading) {
-    return <Loading />;
-  }
-
-  return authenticated ? (<Redirect path="/dashboard" />) : (<Redirect path="/login" />);
+  return <Loading />;
 }
