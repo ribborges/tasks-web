41ae9a69bb4cae5d0f44f59dd4c900703f2c60d4
Richard Borges <ribborges@outlook.com>
2025-03-22 02:10:41 -0300

    refactor: simplify Navbar component by removing unused state and modal logic

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,17 +1,8 @@
 'use client';
 
-import { ReactNode, useState } from 'react';
-import { useRouter } from 'next/navigation';
-import { X, ThreeDots, Calendar2Fill, StarFill, Collection, CollectionFill, HouseFill, PlusSquareFill } from 'react-bootstrap-icons';
+import { ReactNode } from 'react';
 import { clsx } from 'clsx';
 
-import { Spacer } from '@/components/Separator';
-import useModal from '@/hooks/useModal';
-import { useCategoryStore } from '@/lib/store';
-import { Loading } from '@/components/Loading';
-import { AddTask } from '@/components/Task';
-import { Toggle } from '@/components/Input';
-import { AddCategory } from '@/components/Category';
 
 interface ItemContainerProps {
     className?: string;
@@ -19,27 +10,10 @@ interface ItemContainerProps {
 }
 
 export default function Navbar(props: ItemContainerProps) {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
-
-    const { show } = useModal();
-
-    const addModal = () => {
-        show({
-            title: 'Add',
-            content: <Toggle id='add' itemsAlign="baseline" items={[
-                { label: 'Task', content: <AddTask /> },
-                { label: 'Category', content: <AddCategory /> }
-            ]} />
-        });
-    }
-
-    const router = useRouter();
-
     return (
         <>
             <nav className={clsx(`
-                absolute lg:static z-10 h-12 lg:h-auto top-[90%]
+                absolute lg:static z-10 lg:h-auto bottom-0 right-0 left-0
                 backdrop-blur-xs lg:backdrop-blur-none
 				flex lg:flex-col items-center content-center justify-between
 				bg-zinc-100/40 dark:bg-zinc-950/40
@@ -55,21 +29,6 @@ export default function Navbar(props: ItemContainerProps) {
                     </NavItemContainer>
                 </div>
             </nav>
-            {isMenuOpen && (
-                <div className="lg:hidden fixed z-40 backdrop-blur-md">
-                    <div className="flex items-center p-2">
-                        <NavItem onClick={toggleMenu} icon={
-                            isMenuOpen ? <X /> : <ThreeDots />
-                        } />
-                    </div>
-                    <Spacer space={10} />
-                    <div className="flex flex-col items-center justify-center h-full space-y-8">
-                        <NavItemContainer className='flex-col'>
-                            <CategoryList />
-                        </NavItemContainer>
-                    </div>
-                </div>
-            )}
         </>
     );
 }
@@ -82,41 +41,5 @@ export function NavItemContainer(props: { className?: string, children: ReactNod
         )}>
             {props.children}
         </div>
-    );
-}
-
-export function NavItem(props: { icon: any, iconColor?: string, onClick?: () => void, className?: string, label?: string }) {
-    return (
-        <button type='button' onClick={props.onClick} className={clsx(
-            `
-                flex flex-col items-center content-center gap-1 flex-1
-                p-2 lg:p-3
-                hover:bg-zinc-400 dark:hover:bg-zinc-800
-                hover:no-underline
-                border border-solid border-transparent rounded-2xl hover:border-zinc-500 dark:hover:border-zinc-700
-                transition duration-500
-                cursor-pointer
-            `, props.className || ""
-        )}>
-            {props.icon ? <div style={{ color: props.iconColor }} className="text-base md:text-lg lg:text-xl">{props.icon}</div> : <></>}
-            {props.label ? <i className="text-zinc-900 dark:text-zinc-200 not-italic text-[0.6rem]">{props.label}</i> : <></>}
-        </button>
-    );
-}
-
-function CategoryList() {
-    const { categories } = useCategoryStore();
-
-    const router = useRouter();
-
-    return (
-        <>
-            {
-                !categories ? <Loading /> :
-                    categories?.map((category, index) => (
-                        <NavItem key={index} iconColor={category.color} onClick={() => router.push(`/dashboard/category?name=${category.name}`)} icon={<Collection />} label={category.name} />
-                    ))
-            }
-        </>
     );
 }
