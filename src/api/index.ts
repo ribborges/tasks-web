bcab373db2833a062b8e421900e06325a74ed9db
Richard Borges <ribborges@outlook.com>
2025-03-22 14:18:29 -0300

    feat: add request interceptor to include token from cookies in API requests

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,24 @@
+import { cookies } from "next/headers";
 import axios from "axios";
 
 import { apiURL } from "@/config/env";
 
 const api = axios.create({
   baseURL: apiURL,
+  withCredentials: true
+});
+
+api.interceptors.request.use(async (config) => {
+  if (typeof window === 'undefined') {
+    const cookieStore = await cookies();
+    const token = cookieStore.get('token')?.value;
+    
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  }
+  
+  return config;
 });
 
 export { api };
