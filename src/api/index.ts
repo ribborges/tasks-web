328f8a570bcc935c5a900faba3198863f2d70787
Richard Borges <ribborges@outlook.com>
2025-03-22 14:30:01 -0300

    refactor: remove request interceptor for token handling in API requests

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,3 @@
-import { cookies } from "next/headers";
 import axios from "axios";
 
 import { apiURL } from "@/config/env";
@@ -6,19 +5,6 @@ import { apiURL } from "@/config/env";
 const api = axios.create({
   baseURL: apiURL,
   withCredentials: true
-});
-
-api.interceptors.request.use(async (config) => {
-  if (typeof window === 'undefined') {
-    const cookieStore = await cookies();
-    const token = cookieStore.get('token')?.value;
-    
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-  }
-  
-  return config;
 });
 
 export { api };
