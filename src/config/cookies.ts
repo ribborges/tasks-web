cb6f33c222636539fe74f70d3fb4942fdf2f5e26
Richard Borges <ribborges@outlook.com>
2025-03-22 14:52:14 -0300

    feat: add domain option to cookie settings for improved cross-domain handling

--- src/config/cookies.ts
+++ src/config/cookies.ts
@@ -1,12 +1,15 @@
 import { ResponseCookie } from "next/dist/compiled/@edge-runtime/cookies";
 
+import { apiURL } from "./env";
+
 const cookieOpt: Partial<ResponseCookie> = {
     path: "/",
     httpOnly: true,
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
     sameSite: "none",
     secure: true,
-    partitioned: true
+    partitioned: true,
+    domain: apiURL
 };
 
 export { cookieOpt };
