8243db4c933e5702864050bc18183a81acd24d39
Richard Borges <ribborges@outlook.com>
2025-03-22 15:04:34 -0300

    refactor: simplify cookie options by removing unused domain attribute

--- src/config/cookies.ts
+++ src/config/cookies.ts
@@ -1,14 +1,11 @@
 import { ResponseCookie } from "next/dist/compiled/@edge-runtime/cookies";
 
-import { apiURL } from "./env";
-
 const cookieOpt: Partial<ResponseCookie> = {
     path: "/",
     httpOnly: true,
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
     sameSite: "none",
-    secure: true,
-    domain: apiURL
+    secure: true
 };
 
 export { cookieOpt };
