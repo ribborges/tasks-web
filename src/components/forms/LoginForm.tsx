da0a1e701ecdae20fab58751ca724ebc28c0a631
Richard Borges <ribborges@outlook.com>
2025-03-22 02:09:28 -0300

    refactor: remove legacy Login and Register components; introduce new form components with improved validation

---
+++ src/components/forms/LoginForm.tsx
@@ -0,0 +1,51 @@
+'use client';
+
+import { useActionState, useState } from "react";
+import { KeyFill, PersonBadgeFill } from "react-bootstrap-icons";
+
+import { Button, Input } from "@/components/Input";
+import { handleInputChange } from "@/utils/handleInputChange";
+import { Spinner } from "@/components/Loading";
+import { login } from "@/actions/auth";
+import Validation from "@/components/Validation";
+
+export default function LoginForm() {
+    const [state, action, pending] = useActionState(login, undefined);
+
+    const [credentials, setCredentials] = useState({
+        username: '',
+        password: '',
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => handleInputChange(e, setCredentials);
+
+    return (
+        <form action={action} className="flex flex-col gap-2">
+            <Input
+                id="username"
+                value={credentials.username}
+                onChange={handleChange}
+                type="text"
+                name="username"
+                label="Username:"
+                icon={<PersonBadgeFill />}
+                placeholder="ana.Silva"
+            />
+            <Validation error={state?.errors?.username} />
+            <Input
+                id="password"
+                value={credentials.password}
+                onChange={handleChange}
+                type="password"
+                name="password"
+                label="Password:"
+                icon={<KeyFill />}
+                placeholder="••••••••"
+            />
+            <Validation error={state?.errors?.password} />
+            <Button disabled={pending} type="submit">
+                {pending ? <Spinner size={24} /> : "Login"}
+            </Button>
+        </form>
+    )
+}
---
+++ src/components/forms/RegisterForm.tsx
@@ -0,0 +1,75 @@
+'use client';
+
+import { useActionState, useState } from "react";
+import { EnvelopeFill, KeyFill, PersonBadgeFill, PersonFill } from "react-bootstrap-icons";
+
+import { Button, Input } from "@/components/Input";
+import { handleInputChange } from "@/utils/handleInputChange";
+import { Spinner } from "@/components/Loading";
+import { register } from "@/actions/auth";
+import Validation from "@/components/Validation";
+
+export default function RegisterForm() {
+    const [state, action, pending] = useActionState(register, undefined);
+
+    const [user, setUser] = useState({
+        name: '',
+        username: '',
+        email: '',
+        password: '',
+    });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => handleInputChange(e, setUser);
+
+    return (
+        <form action={action} className="flex flex-col gap-2">
+            <Input
+                id="name"
+                value={user.name}
+                onChange={handleChange}
+                type="text"
+                name="name"
+                label="Full name:"
+                icon={<PersonFill />}
+                placeholder="Ana Silva"
+            />
+            <Validation error={state?.errors?.name} />
+            <Input
+                id="username"
+                value={user.username}
+                onChange={handleChange}
+                type="text"
+                name="username"
+                label="Username:"
+                icon={<PersonBadgeFill />}
+                placeholder="ana.Silva"
+            />
+            <Validation error={state?.errors?.username} />
+            <Input
+                id="email"
+                value={user.email}
+                onChange={handleChange}
+                type="email"
+                name="email"
+                label="Email:"
+                icon={<EnvelopeFill />}
+                placeholder="ana_silva@email.com"
+            />
+            <Validation error={state?.errors?.email} />
+            <Input
+                id="password"
+                value={user.password}
+                onChange={handleChange}
+                type="password"
+                name="password"
+                label="Password:"
+                icon={<KeyFill />}
+                placeholder="••••••••"
+            />
+            <Validation error={state?.errors?.password} />
+            <Button disabled={pending} type="submit">
+                {pending ? <Spinner size={24} /> : "Register"}
+            </Button>
+        </form>
+    )
+}
