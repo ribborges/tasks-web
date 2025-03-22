5409a86d73a73d4120dfc759e9f1aec6d855cbb8
Richard Borges <ribborges@outlook.com>
2025-03-22 14:52:29 -0300

    fix: remove partitioned attribute from cookie options

--- src/config/cookies.ts
+++ src/config/cookies.ts
@@ -8,7 +8,6 @@ const cookieOpt: Partial<ResponseCookie> = {
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
     sameSite: "none",
     secure: true,
-    partitioned: true,
     domain: apiURL
 };
 
