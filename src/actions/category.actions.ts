3980916fce4ecc04998b5a2d7d4b452b0889f0e4
Richard Borges <ribborges@outlook.com>
2025-04-22 02:13:25 -0300

    feat: implement newCategory function for category creation with validation

--- src/actions/category.actions.ts
+++ src/actions/category.actions.ts
@@ -2,7 +2,8 @@
 
 import { cookies } from "next/headers";
 
-import { getCategories } from "@/services/category.service";
+import { createCategory, getCategories } from "@/services/category.service";
+import { FormState } from "@/lib/definitions";
 
 export async function fetchCategories(userId: string) {
     const cookieStore = cookies();
@@ -29,6 +30,44 @@ export async function fetchCategories(userId: string) {
             } else {
                 return [];
             }
+        }).catch((error) => {
+            return { message: "An error occurred", error: true };
+        });
+}
+
+export async function newCategory(state: FormState, formData: FormData): Promise<FormState> {
+    const cookieStore = cookies();
+    const tokenCookie = (await cookieStore).get("token");
+
+    if (!tokenCookie) {
+        return { message: "No token found", errors: { token: "Token is missing" } };
+    }
+
+    const validatedFields = state.NewCategoryFormSchema.safeParse({
+        name: formData.get("name"),
+        color: formData.get("color"),
+    });
+
+    if (!validatedFields.success) {
+        return {
+            errors: {
+                name: validatedFields.error.formErrors.fieldErrors.name,
+                color: validatedFields.error.formErrors.fieldErrors.color,
+            },
+        };
+    }
+
+    return await createCategory(validatedFields.data, tokenCookie.value)
+        .then((res) => {
+            if (!res) {
+                return { message: "An error occurred", error: true };
+            }
+
+            if (!res?.status.toString().startsWith("2")) {
+                return { message: res.status + ": " + res.data, error: true };
+            }
+
+            return res.data;
         }).catch((error) => {
             return { message: "An error occurred", error: true };
         });
