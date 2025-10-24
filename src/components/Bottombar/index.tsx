daecb8901d1b491ba4eeeb0510fd1b9c550e3242
Richard <46366493+ribborges@users.noreply.github.com>
2025-10-24 16:57:26 -0300

    refactor: simplify Bottombar component layout by removing unnecessary container and adjusting styles

--- src/components/Bottombar/index.tsx
+++ src/components/Bottombar/index.tsx
@@ -12,9 +12,9 @@ export default function Bottombar(props: ItemContainerProps) {
     return (
         <>
             <nav className={clsx(`
-                absolute lg:static z-10 lg:h-auto bottom-0 right-0 left-0
-                backdrop-blur-xs lg:backdrop-blur-none
-				flex lg:flex-col items-center content-center justify-between
+                absolute z-10 bottom-0 self-center
+                backdrop-blur-sm
+				flex items-center content-center gap-5
 				bg-zinc-100/40 dark:bg-zinc-950/40
 				p-3 m-2
 				border border-b-2 border-solid rounded-full lg:rounded-4xl
@@ -22,25 +22,10 @@ export default function Bottombar(props: ItemContainerProps) {
 				shadow-xl
 				select-none
 			`, props.className)}>
-                <div className="flex lg:flex-col flex-1 items-center content-between gap-5">
-                    <BottombarItemContainer className='justify-between flex-1 lg:flex-none'>
-                        {props.children}
-                    </BottombarItemContainer>
-                </div>
+                {props.children}
             </nav>
         </>
     );
 }
 
-function BottombarItemContainer(props: { className?: string, children: ReactNode }) {
-    return (
-        <div className={clsx(
-            "flex flex-row lg:flex-col gap-2",
-            props.className || ''
-        )}>
-            {props.children}
-        </div>
-    );
-}
-
-export { Bottombar, BottombarItemContainer };
+export { Bottombar };
